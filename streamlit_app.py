ae05df2e7727e5b5e6c341f005e64eb37f537ba0
ManeruAkhila <115208657+Maneruakhila@users.noreply.github.com>
2022-10-07 12:38:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,17 @@ streamlit.header('Breafast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale,Spinach $ Rocket Smoothie')
 streamlit.text('Hard-boiled Free-Raange Egg')
-streamlit.text('Avocado toast')
+
 
 
 streamlit.header('Build Your Own Fruit Smoothie')
+
+
+streamlit.header('Breafast Fovorites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach $ Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Raange Egg')
+streamlit.text('Avocado toast')
 
 
 
