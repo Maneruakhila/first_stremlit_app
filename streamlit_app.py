f276210fd0cd19639182e8f6e72a7b6e5963e7a3
ManeruAkhila <115208657+Maneruakhila@users.noreply.github.com>
2022-10-07 12:46:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,12 @@ streamlit.header('Breafast Fovorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale,Spinach $ Rocket Smoothie')
 streamlit.text('Hard-boiled Free-Raange Egg')
-streamlit.text('Avocado toast')
+streamlit.text('avocado toast')
 
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
 
